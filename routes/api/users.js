2c5a96c5d4b14f796357265a08883ef14df9b0a0
Denis Rajlic <denis.rajlic@gmail.com>
2020-12-11 13:28:30 +0100

    connect db & setup user routes

---
+++ routes/api/users.js
@@ -0,0 +1,64 @@
+const express = require('express');
+
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
+
+// @route     POST api/users
+// @desc      Register user
+// @access    Public
+router.post('/', [
+  check('name', 'Name is required').not().isEmpty(),
+  check('email', 'Please include a valid email,').isEmail(),
+  check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 }),
+],
+async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+
+  const { name, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+
+    if (user) return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+
+    user = new User({
+      name,
+      email,
+      password,
+    });
+
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(password, salt);
+
+    // Save the user to the DB
+    await user.save();
+
+    const payload = {
+      user: {
+        id: user.id, // with mongoose there's an abstraction, we don't have to use user._id
+      },
+    };
+
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: 3600 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      },
+    );
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+module.exports = router;
