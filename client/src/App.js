73b358d729e5404f120207ac182865179549b398
Denis Rajlic <denis.rajlic@gmail.com>
2021-01-11 23:24:18 +0100

    output leaderboards in browser when collction changes

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+const ENDPOINT = "http://localhost:5000";
+
+function App() {
+  const [response, setResponse] = useState("");
+
+  useEffect(() => {
+    const socket = io.connect(ENDPOINT);
+
+    socket.on('updateScore', (data) => {
+      console.log(data);
+    });
+
+    // CLEAN UP THE EFFECT
+    return () => socket.disconnect();
+  }, []);
+
+  return (
+    <p>
+      It's <time dateTime={response}>{response}</time>
+    </p>
+  );
+}
+
+export default App;
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
+const User = require('./models/User');
+
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
 
 // Connect database
 connectDB();
@@ -18,6 +27,25 @@ app.use('/api/games', require('./routes/api/games'));
 app.use('/api/teams', require('./routes/api/teams'));
 app.use('/api/results', require('./routes/api/results'));
 
+const changeStream = User.watch();
+
+// Run when client connects
+io.on('connection', socket => {
+  console.log('New client connected');
+
+  changeStream.on('change', async change => {
+    console.log('Collection changed');
+    const results = await User.find().select(['-password', '-email', '-date', '-_id', '-__v']).sort({ xp: -1 });
+    socket.emit('updateScore', results);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+module.exports = io;
